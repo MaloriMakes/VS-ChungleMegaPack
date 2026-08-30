0f71560ecfb44381b912041a6695d6a7a595e344
Malorie <Kaziliisb@gmail.com>
2026-08-30 16:55:28 -0700

    Then god created a cat
    • catsby now exists...sorta

---
+++ catsbyrdx-modlink/Setup.bat
@@ -0,0 +1,11 @@
+set "species=catsby"
+set "model=catsby-faceless"
+set "backDir=C:\VS-Backdrop"
+set "workDir=%~dp0
+
+mkdir C:\VS-Backdrop
+mkdir %appdata%\Vintagestory\assets\survival\textures\entity\humanoid\seraphskinparts\body
+
+mklink /D %appdata%\VintagestoryData\mods\%species%rdx-modlink "%workDir%"
+mklink %backDir%\%species%-backdrop.json "%workDir%\assets\%species%rdx\shapes\entity\humanoid\%model%.json"
+mklink %appdata%\Vintagestory\assets\survival\textures\entity\humanoid\seraphskinparts\body\%species%bodytex.png "%workDir%\assets\%species%rdx\textures\entity\humanoid\seraphskinparts\body\%species%bodytex.png"
